5877b055594d547cc1d08f6f078d40735c5316b7
Adrian <adrian.hamzah15@gmail.com>
2024-04-17 21:12:19 +0800

    feat: add sonarcloud

--- src/main/java/id/ac/ui/cs/advprog/payment/controller/CController.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/controller/CController.java
@@ -1,19 +1,16 @@
 package id.ac.ui.cs.advprog.payment.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/")
-public class cController {
+public class CController {
     String createHTML = "userCreate";
     @GetMapping("/")
     @ResponseBody
     public String createUserPage(Model model) {
-        return "<h1>haloo berhasil ya</h1>";
+        return "<h1>AFK-3's Payment</h1>";
     }
 }
