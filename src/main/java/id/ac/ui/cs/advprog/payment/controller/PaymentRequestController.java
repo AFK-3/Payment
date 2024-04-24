3928fc447dfbf30cc15ad1e6a8c90ac8e9db8bae
Adrian <adrian.hamzah15@gmail.com>
2024-04-24 10:15:47 +0700

    feat: add createPaymentRequest

--- src/main/java/id/ac/ui/cs/advprog/payment/controller/PaymentRequestController.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/controller/PaymentRequestController.java
@@ -1,20 +1,38 @@
 package id.ac.ui.cs.advprog.payment.controller;
 
-import id.ac.ui.cs.advprog.payment.model.PaymentRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import id.ac.ui.cs.advprog.payment.service.PaymentRequestService;
+import id.ac.ui.cs.advprog.payment.model.PaymentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/payment-request")
 public class PaymentRequestController {
     private PaymentRequestService paymentRequestService;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public PaymentRequestController(PaymentRequestService paymentRequestService, ObjectMapper objectMapper) {
+        this.paymentRequestService = paymentRequestService;
+        this.objectMapper = objectMapper;
+    }
 
     @PostMapping("/create")
-    public String createPaymentRequest(@ModelAttribute("form")PaymentRequest paymentRequest) {
-        System.out.println(paymentRequest);
-        return "";
+    public ResponseEntity<String> createPaymentRequest(@RequestBody PaymentRequest paymentRequest) {
+        try {
+            String paymentRequestJson = objectMapper.writeValueAsString(paymentRequest);
+            String responseJson = "{\"paymentRequest\":" + paymentRequestJson + "}";
+            return ResponseEntity.ok(responseJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while processing payment request");
+        }
     }
 }
