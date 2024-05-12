4c8ee8253e7d97a8c51122403bc4897f00cf55ca
Adrian <adrian.hamzah15@gmail.com>
2024-05-12 16:28:40 +0700

    feat: add threadpool

--- src/main/java/id/ac/ui/cs/advprog/payment/PaymentApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/payment/PaymentApplication.java
@@ -15,4 +15,15 @@ public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
+
+    @Bean("threadPoolTaskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("thread");
+        executor.initialize();
+        return executor;
+    }
 }
