3d8a61373479aefa7cb01d32c1ef484dd1abfa25
Adrian <adrian.hamzah15@gmail.com>
2024-05-26 23:18:26 +0700

    fix: last commit for test coverage

--- src/test/java/id/ac/ui/cs/advprog/payment/controller/PaymentRequestControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/payment/controller/PaymentRequestControllerTest.java
@@ -87,6 +87,18 @@ public class PaymentRequestControllerTest {
         when(paymentRequestService.create(any(PaymentRequest.class))).thenReturn(paymentRequest);
 
         mockMvc.perform(post("/payment-request/create")
+                        .header("Authorization", BUYER_TOKEN+"aa11122a")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"paymentAmount\":100}"))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(post("/payment-request/create")
+                        .header("Authorization", STAFF_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"paymentAmount\":100}"))
+                .andExpect(status().isForbidden());
+
+        mockMvc.perform(post("/payment-request/create")
                         .header("Authorization", BUYER_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"paymentAmount\":100}"))
@@ -111,6 +123,14 @@ public class PaymentRequestControllerTest {
         String token = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJha3UiLCJpYXQiOjE3MTY0NDc2NTIsImV4cCI6MTcxNjQ1MTI1Mn0.ITlXCYR0blT-CAfpt5Jv7Nwr0mrchixeVvyY4QS9yyn9WWUKuWlgGQRDsww2YLyb81syblf5o02-Z9_Uu-SWdQ";
 
         mockMvc.perform(get("/payment-request/get-all-by-buyer-username")
+                .header("Authorization", BUYER_TOKEN+"AAAxxx"))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(get("/payment-request/get-all-by-buyer-username")
+                        .header("Authorization", STAFF_TOKEN))
+                .andExpect(status().isForbidden());
+
+        mockMvc.perform(get("/payment-request/get-all-by-buyer-username")
                 .header("Authorization", BUYER_TOKEN))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"paymentsRequest\":[{\"id\":\"7ac424ea-c319-4793-9346-c89b40dd2984\",\"paymentStatus\":\"WAITING_RESPONSE\",\"paymentAmount\":61,\"paymentRequestTime\":27325000,\"paymentResponseTime\":null,\"buyerUsername\":\"aku\"}]}"));
@@ -130,9 +150,21 @@ public class PaymentRequestControllerTest {
         when(paymentRequestService.deleteById("7ac424ea-c319-4793-9346-c89b40dd2984")).thenReturn(paymentRequest);
 
         mockMvc.perform(delete("/payment-request/delete-by-id/7ac424ea-c319-4793-9346-c89b40dd2984")
+                        .header("Authorization", BUYER_TOKEN+"s"))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(delete("/payment-request/delete-by-id/7ac424ea-c319-4793-9346-c89b40dd2984")
+                        .header("Authorization", STAFF_TOKEN))
+                .andExpect(status().isForbidden());
+
+        mockMvc.perform(delete("/payment-request/delete-by-id/7ac424ea-c319-4793-9346-c89b40dd2984")
                         .header("Authorization", BUYER_TOKEN))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"deletedPaymentsRequest\":{\"id\":\"7ac424ea-c319-4793-9346-c89b40dd2984\",\"paymentStatus\":\"WAITING_RESPONSE\",\"paymentAmount\":61,\"paymentRequestTime\":27325000,\"paymentResponseTime\":null,\"buyerUsername\":\"aku\"}}"));
+
+        mockMvc.perform(delete("/payment-request/delete-by-id/7ac424ea-c319-4793-9346-c89b40d")
+                        .header("Authorization", BUYER_TOKEN))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -147,19 +179,38 @@ public class PaymentRequestControllerTest {
         when(paymentRequestService.findById("7ac424ea-c319-4793-9346-c89b40dd2984")).thenReturn(paymentRequest);
 
         mockMvc.perform(put("/payment-request/cancel/7ac424ea-c319-4793-9346-c89b40dd2984")
+                        .header("Authorization", BUYER_TOKEN+"ss"))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(put("/payment-request/cancel/7ac424ea-c319-4793-9346-c89b40dd2984")
+                .header("Authorization", STAFF_TOKEN))
+                .andExpect(status().isForbidden());
+
+        mockMvc.perform(put("/payment-request/cancel/7ac424ea-c319-4793-9346-c89b40dd2984")
                         .header("Authorization", BUYER_TOKEN))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"canceledPaymentsRequest\":{\"id\":\"7ac424ea-c319-4793-9346-c89b40dd2984\",\"paymentStatus\":\"CANCELLED\",\"paymentAmount\":61,\"paymentRequestTime\":27325000,\"paymentResponseTime\":null,\"buyerUsername\":\"aku\"}}"));
+
+        mockMvc.perform(put("/payment-request/cancel/7ac424ea-c319-4793-9346-c89b40d")
+                        .header("Authorization", BUYER_TOKEN))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
     public void testRespondPaymentRequest() throws Exception {
         when(paymentRequestService.findById("7ac424ea-c319-4793-9346-c89b40dd2984")).thenReturn(paymentRequest);
 
-
         mockMvc.perform(put("/payment-request/respond/7ac424ea-c319-4793-9346-c89b40dd2984/ACCEPT")
                         .header("Authorization", STAFF_TOKEN))
                 .andExpect(status().isOk());
+
+        mockMvc.perform(put("/payment-request/respond/7ac424ea-c319-4793-9346-c89b40dd2984/REJECT")
+                        .header("Authorization", STAFF_TOKEN))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put("/payment-request/respond/7ac424ea-c319-4793-9346-c89b40dd2984/ACCEPT2")
+                        .header("Authorization", STAFF_TOKEN))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -177,6 +228,11 @@ public class PaymentRequestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedJson));
+
+        mockMvc.perform(put("/payment-request/edit/7ac424ea-c319-4793-9346-c89b40/" + newAmount)
+                        .header("Authorization", BUYER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
     @Configuration
